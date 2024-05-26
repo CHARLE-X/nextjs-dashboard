904853fb1b2f5fb504fc346bc31fde99e847485e
Ebube Charles-Monwuba <ebubechukwu charles-monwuba>
2024-05-26 17:52:09 +0100

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const customerspage = () => {
+  return (
+    <div>customers page</div>
+  )
+}
+
+export default customerspage
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+    return <p>Dashboard Page</p>;
+  }
+
+// import React from 'react'
+
+// const page = () => {
+//   return (
+//     <div>
+//         <p>
+//             Dashboard page
+//         </p>
+//     </div>
+//   )
+// }
+
+// export default page
